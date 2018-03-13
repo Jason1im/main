3702e2b7d424d61c4dd774d40aba7064150e1b36
Jason1im <ezstead@gmail.com>
2018-03-13 22:18:55 +0800

    made edits to conform with coding standards.

--- src/test/java/seedu/address/logic/commands/ClearHistoryCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ClearHistoryCommandTest.java
@@ -10,36 +10,37 @@ import seedu.address.logic.UndoRedoStack;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
-    public class ClearHistoryCommandTest {
-        private ClearHistoryCommand clearHistoryCommand;
-        private CommandHistory history;
+public class ClearHistoryCommandTest {
+    private ClearHistoryCommand clearHistoryCommand;
+    private CommandHistory history;
 
-        @Before
-        public void setUp() {
-            Model model = new ModelManager();
-            history = new CommandHistory();
-            clearHistoryCommand = new ClearHistoryCommand();
-            clearHistoryCommand.setData(model, history, new UndoRedoStack());
-        }
-
-        @Test
-        public void execute() {
-            assertCommandResult(clearHistoryCommand, ClearHistoryCommand.MESSAGE_SUCCESS);
-
-            String command2 = "randomCommand";
-            String command3 = "select 1";
-            history.add(command2);
-            history.add(command3);
-
-            String expectedMessage = ClearHistoryCommand.MESSAGE_SUCCESS;
-
-            assertCommandResult(clearHistoryCommand, expectedMessage);
-        }
-
-        /**
-         * Asserts that the result message from the execution of {@code clearHistoryCommand} equals to {@code expectedMessage}
-         */
-        private void assertCommandResult(ClearHistoryCommand clearHistoryCommand, String expectedMessage) {
-            assertEquals(expectedMessage, clearHistoryCommand.execute().feedbackToUser);
-        }
+    @Before
+    public void setUp() {
+        Model model = new ModelManager();
+        history = new CommandHistory();
+        clearHistoryCommand = new ClearHistoryCommand();
+        clearHistoryCommand.setData(model, history, new UndoRedoStack());
     }
+
+    @Test
+    public void execute() {
+        assertCommandResult(clearHistoryCommand, ClearHistoryCommand.MESSAGE_SUCCESS);
+
+        String command2 = "randomCommand";
+        String command3 = "select 1";
+        history.add(command2);
+        history.add(command3);
+
+        String expectedMessage = ClearHistoryCommand.MESSAGE_SUCCESS;
+
+        assertCommandResult(clearHistoryCommand, expectedMessage);
+    }
+
+    /**
+     * Asserts that the result message from the execution of {@code clearHistoryCommand}
+     * is equal to {@code expectedMessage}
+     */
+    private void assertCommandResult(ClearHistoryCommand clearHistoryCommand, String expectedMessage) {
+        assertEquals(expectedMessage, clearHistoryCommand.execute().feedbackToUser);
+    }
+}
