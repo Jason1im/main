8fb771e4fcdcd6765e8125e6eb9d47c989017441
Jason1im <ezstead@gmail.com>
2018-03-21 02:05:56 +0800

    Fix code styling

--- src/test/java/seedu/address/logic/commands/LoginCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LoginCommandTest.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
@@ -7,13 +11,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
-
 
 /**
  * Contains integration tests (interaction with the Model) and unit tests for ListCommand.
--- src/test/java/seedu/address/logic/commands/LogoutCommandTest.java
+++ src/test/java/seedu/address/logic/commands/LogoutCommandTest.java
@@ -1,5 +1,9 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -7,12 +11,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
-
-
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 public class LogoutCommandTest {
 
