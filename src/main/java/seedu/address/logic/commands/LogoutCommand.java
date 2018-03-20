34737f1aeb8d4baf90fc8de5fb9796fef4ebfec8
Jason1im <ezstead@gmail.com>
2018-03-20 22:04:09 +0800

    Added new login and logout commands.

---
+++ src/main/java/seedu/address/logic/commands/LogoutCommand.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.exceptions.UserLogoutException;
+
+import static java.util.Objects.requireNonNull;
+
+public class LogoutCommand extends Command{
+    public static final String COMMAND_WORD = "logout";
+    public static final String MESSAGE_SUCCESS = "You have logout successfully!";
+    public static final String MESSAGE_MULTIPLE_LOGOUT = "You have already logout.";
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        requireNonNull(model);
+        try {
+            model.logout();
+            return new CommandResult(MESSAGE_SUCCESS);
+        } catch (UserLogoutException ule) {
+            throw new CommandException(MESSAGE_MULTIPLE_LOGOUT);
+        }
+    }
+}
