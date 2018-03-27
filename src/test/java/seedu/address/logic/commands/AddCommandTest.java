915d0f14feb73fed9e39a3cd6a5554184fded5b4
Jason Lim <ezstead@gmail.com>
2018-03-28 02:45:58 +0800

    Update AddCommandTest.java
    fix error

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -180,7 +180,7 @@ public class AddCommandTest {
         public void addPerson(Person person) throws DuplicatePersonException {
             throw new DuplicatePersonException();
         }
-
+        
         @Override
         public ReadOnlyAddressBook getAddressBook() {
             return new AddressBook();
