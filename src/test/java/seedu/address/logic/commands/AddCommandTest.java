101453d805a9e27c5a43307f1245b00b7f230512
Jason Lim <ezstead@gmail.com>
2018-03-28 03:04:44 +0800

    Remove whitespace

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -164,7 +164,7 @@ public class AddCommandTest {
 
         @Override
         public void register(String username, String password){ fail("This method should not be called."); }
- 
+
         @Override
         public void updateFilteredJobList(Predicate<Job> predicate) {
             fail("This method should not be called.");
