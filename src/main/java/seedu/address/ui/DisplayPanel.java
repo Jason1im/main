aac9440f4d163f0a666c1e4097a7079176f40d21
Jason1im <ezstead@gmail.com>
2018-04-10 08:50:52 +0800

    Added Login UI

---
+++ src/main/java/seedu/address/ui/DisplayPanel.java
@@ -0,0 +1,16 @@
+package seedu.address.ui;
+
+import javafx.scene.layout.Region;
+
+/**
+ * A display page for login window.
+ */
+public class DisplayPanel extends UiPart<Region> {
+
+    private static final String FXML = "DisplayPanel.fxml";
+
+    public DisplayPanel() {
+        super(FXML);
+    }
+
+}
